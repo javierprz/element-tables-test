2624b3722815defe5ee49ac4eae1e0bab8a64107
Javier Pérez Arias <javier@iMac.home>
2020-02-09 12:14:24 +0100

    feat(first commit): Table with multiple selection and sorting.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,56 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div style="padding-left:20px; padding-right: 20px;">
+      <el-table
+        :data="tableData"
+        style="width: 100%;"
+        v-on:sort-change="sortChange">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="Exercise"
+          sortable="custom"
+          style="width: 80%">
+        </el-table-column>
+        <el-table-column
+          prop="vessel_type"
+          label="Vesel type"
+          sortable="custom"
+          style="width: 20%">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
+import orderBy from 'lodash/orderBy'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      tableData: [{
+        title: 'Deck Inspection',
+        vessel_type: 'General Cargo Ship'
+      }, {
+        title: 'Underwater Inspection',
+        vessel_type: 'General Cargo Ship'
+      }]
+    }
+  },
+  methods: {
+    sortChange (event) {
+      console.log(event)
+      let order = event.order === 'descending' ? 'desc' : 'asc'
+      this.tableData = orderBy(this.tableData, [event.prop], [order])
+    }
   }
 }
 </script>
